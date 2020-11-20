1c19ffa54d04d1633328778069794b85b3cae774
AFEDOROVK <afedorovkaritsky@tsconsulting.ru>
2020-11-20 12:00:32 +0300

    JavaCore_01

--- src/JavaCore_05/Car.java
+++ src/JavaCore_05/Car.java
@@ -4,27 +4,23 @@ class Car {
     private int yearOfIssue;
     private String carColor;
     private String carModel;
-
+//сеттеры
     public void setYear(int year){
         yearOfIssue = year;
     }
-
-    public int getYearOfIssue() {
-        return yearOfIssue;
-    }
-
     public void setColor(String color){
         carColor = color;
     }
-
-    public String getColor() {
-        return carColor;
-    }
-
     public void setModel(String model){
         carModel = model;
     }
-
+//геттеры
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+    public String getColor() {
+        return carColor;
+    }
     public String getModel() {
         return carModel;
     }
